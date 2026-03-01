ea4f6b9b1701f72b8808b9b9a5dd6660b7ae266d
sunabhaghosh-cloud <sunabhaghosh@gmail.com>
2026-03-02 00:17:52 +0530

    first commit

---
+++ uc3.java
@@ -0,0 +1,10 @@
+public class uc3 {
+    public static void main(String[] args) {
+        System.out.println(String.join(" ", " ***  ", " ***  ", " *****", " *****"));
+        System.out.println(String.join(" ", "** ** ", "** ** ", "**  **", "**   "));
+        System.out.println(String.join(" ", "** ** ", "** ** ", "***** ", "*****"));
+        System.out.println(String.join(" ", "** ** ", "** ** ", "**    ", "    **"));
+        System.out.println(String.join(" ", "** ** ", "** ** ", "**    ", "    **"));
+        System.out.println(String.join(" ", " ***  ", " ***  ", "**    ", " *** "));
+    }
+}
